742b41ed2968456e2c8b59fc1bfb9c8512213250
SAISURYA <saisurya015@gmail.com>
2025-02-17 17:12:27 +0530

    email & password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,8 +18,19 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function(email, password) {
-  
-  const exists = await this.findOne({email}) 
+
+  // validation
+  if(!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if(!validator.isEmail(email)) {
+    throw Error('Email is Not valid')
+  }
+  if(!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
+  const exists = await this.findOne({email})
+
 
   if(exists) {
     throw Error('Email already in use')
